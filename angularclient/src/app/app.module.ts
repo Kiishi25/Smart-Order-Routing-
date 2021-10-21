3b1331d5413d909d6c666de7b29aed35fa9a0983
willharto <willharto@porticode.github.com>
2021-10-21 16:18:54 +0100

    upgrade angular5 to angular12, fix button colour, add pagination

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -4,15 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { TradeComponent } from './trade/trade.component';
+import { TradeSortComponent, TradeComponent } from './trade/trade.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { OrderComponent } from './order/order.component';
+import { OrderSortComponent, OrderComponent } from './order/order.component';
 import { ContactComponent } from './contact-us/contact.component';
-import { TradeHistoryComponent } from './tradeHistory/tradeHistory.component';
+import { TradeHistorySortComponent, TradeHistoryComponent } from './tradeHistory/tradeHistory.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { TradeService } from '../service/trade.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -23,12 +24,16 @@ import { TradeService } from '../service/trade.service';
     TradeHistoryComponent,
     LoginComponent,
     RegisterComponent,
-    ContactComponent
+    ContactComponent,
+    OrderSortComponent,
+    TradeSortComponent,
+    TradeHistorySortComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [TradeService],
   bootstrap: [AppComponent]
--- angularclient/src/app/order/order.component.ts
+++ angularclient/src/app/order/order.component.ts
@@ -1,4 +1,167 @@
-import { Component } from '@angular/core';
+import { Component, Directive, EventEmitter, Input, Output, QueryList, ViewChildren } from '@angular/core';
+
+interface Order {
+  id: number;
+  instrument: string;
+  price: number;
+}
+
+const BUY_ORDERS: Order[] = [
+  {
+    id: 1,
+    instrument: 'BTC-USD',
+    price: 30000,
+  },
+  {
+    id: 2,
+    instrument: 'BTC-EUR',
+    price: 29000,
+  },
+  {
+    id: 3,
+    instrument: 'BTC-GBP',
+    price: 22000,
+  },
+  {
+    id: 4,
+    instrument: 'ETH-USD',
+    price: 43141,
+  },
+  {
+    id: 5,
+    instrument: 'ETH-EUR',
+    price: 40132,
+  },
+  {
+    id: 6,
+    instrument: 'ETH-GBP',
+    price: 34039,
+  },
+  {
+    id: 7,
+    instrument: 'XHL-USD',
+    price: 29183,
+  },
+  {
+    id: 8,
+    instrument: 'XHL-EUR',
+    price: 27314,
+  },
+  {
+    id: 9,
+    instrument: 'XHL-GBP',
+    price: 22019,
+  },
+  {
+    id: 10,
+    instrument: 'NIO-USD',
+    price: 109888,
+  },
+  {
+    id: 11,
+    instrument: 'NIO-EUR',
+    price: 100232,
+  },
+  {
+    id: 12,
+    instrument: 'NIO-GBP',
+    price: 91002,
+  }
+];
+
+const SELL_ORDERS: Order[] = [
+  {
+    id: 1,
+    instrument: 'BTC-USD',
+    price: 35000,
+  },
+  {
+    id: 2,
+    instrument: 'BTC-EUR',
+    price: 31000,
+  },
+  {
+    id: 3,
+    instrument: 'BTC-GBP',
+    price: 24000,
+  },
+  {
+    id: 4,
+    instrument: 'ETH-USD',
+    price: 45222,
+  },
+  {
+    id: 5,
+    instrument: 'ETH-EUR',
+    price: 41345,
+  },
+  {
+    id: 6,
+    instrument: 'ETH-GBP',
+    price: 32972,
+  },
+  {
+    id: 7,
+    instrument: 'XHL-USD',
+    price: 31928,
+  },
+  {
+    id: 8,
+    instrument: 'XHL-EUR',
+    price: 29615,
+  },
+  {
+    id: 9,
+    instrument: 'XHL-GBP',
+    price: 25616,
+  },
+  {
+    id: 10,
+    instrument: 'NIO-USD',
+    price: 111223,
+  },
+  {
+    id: 11,
+    instrument: 'NIO-EUR',
+    price: 105626,
+  },
+  {
+    id: 12,
+    instrument: 'NIO-GBP',
+    price: 97828,
+  }
+];
+
+export type SortColumn = keyof Order | '';
+export type SortDirection = 'asc' | 'desc' | '';
+const rotate: {[key: string]: SortDirection} = { 'asc': 'desc', 'desc': '', '': 'asc' };
+
+const compare = (v1: string | number, v2: string | number) => v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
+
+export interface SortEvent {
+  column: SortColumn;
+  direction: SortDirection;
+}
+
+@Directive({
+  selector: 'th[sortable]',
+  host: {
+    '[class.asc]': 'direction === "asc"',
+    '[class.desc]': 'direction === "desc"',
+    '(click)': 'rotate()'
+  }
+})
+export class OrderSortComponent {
+
+  @Input() sortable: SortColumn = '';
+  @Input() direction: SortDirection = '';
+  @Output() sort = new EventEmitter<SortEvent>();
+
+  rotate() {
+    this.direction = rotate[this.direction];
+    this.sort.emit({column: this.sortable, direction: this.direction});
+  }
+}
 
 @Component({
   selector: 'order-root',
@@ -6,9 +169,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent {
+
   title: String;
+  orders: Order[];
+
+  page = 1;
+  pageSize = 10;
+  collectionSize = BUY_ORDERS.length;
 
   constructor() {
     this.title = "Orders";
+    this.orders = BUY_ORDERS;
+
+    this.refreshOrders();
+  }
+
+  @ViewChildren(OrderSortComponent) headers: QueryList<OrderSortComponent>;
+
+  onSort({column, direction}: SortEvent) {
+    // resetting other headers
+    this.headers.forEach(header => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    // sorting orders
+    if (direction === '' || column === '') {
+      this.orders = BUY_ORDERS;
+    } else {
+      this.orders = [...BUY_ORDERS].sort((a, b) => {
+        const res = compare(a[column], b[column]);
+        return direction === 'asc' ? res : -res;
+      });
+    }
+  }
+
+  refreshOrders() {
+    this.orders = BUY_ORDERS
+      .map((order, i) => ({id: i+1, ...order}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
   }
 }
